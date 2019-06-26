c1a97d24a44124af1875fe1879e14446fa432050
Davindar <davdavdav4@gmail.com>
2019-06-26 23:52:21 +0100

    project update

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const Air = {
   "nav": {
     "nav-item-1": "Homepage",
     "nav-item-2": "Team",
-    "nav-item-3": "Contact",
+    "nav-item-3": "App",
     "img-src": "img/logo.png"
   },
   "top": {
@@ -22,5 +22,37 @@ const Circle = document.getElementById("top-img");
 Circle.setAttribute('src', Air["top"]["img-src"]);
 
 
+// Double Click to make grey
+let suttonb = document.querySelector(".top-text h1");
+suttonb.addEventListener("dblclick", (event) => {
+event.target.style.backgroundColor = "gray";
+});
+
+// Mouse Over to change background colour
+let buttons = document.querySelectorAll(".top-text h1");
+buttons[0].addEventListener("mouseover", (event) => {
+  event.target.style.backgroundColor = "black"
+  event.target.style.color = "yellow"
+});
+buttons[0].addEventListener("mouseleave", (event) => {
+    event.target.style.backgroundColor ="yellow" 
+    event.target.style.color = "black";
+  });
+  buttons[1].addEventListener("mouseover", (event) => {
+    event.target.style.backgroundColor = "black"
+    event.target.style.color = "yellow"
+  });
+  buttons[1].addEventListener("mouseleave", (event) => {
+      event.target.style.backgroundColor ="yellow" 
+      event.target.style.color = "black";
+    });
+  buttons[2].addEventListener("mouseover", (event) => {
+      event.target.style.backgroundColor = "black"
+      event.target.style.color = "yellow"
+    });
+  buttons[2].addEventListener("mouseleave", (event) => {
+        event.target.style.backgroundColor ="yellow" 
+        event.target.style.color = "black";
+      });
 
 
